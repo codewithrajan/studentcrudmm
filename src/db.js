863c162a854898201e86561cb17a177568bb948b
Rajan Kumar <rajanrajan06254@gmail.com>
2023-12-28 10:49:20 +0530

    for database

--- src/db.js
+++ src/db.js
@@ -1,8 +1,20 @@
+// const mongoose = require('mongoose');
+
+// async function connectToDatabase() {
+//   try {
+//     await mongoose.connect("mongodb://localhost:27017/abcd"
+//    );
+//     console.log('Connected to MongoDB');
+//   } catch (error) {
+//     console.error('Error connecting to MongoDB:', error);
+//   }
+// }
+// module.exports = { connectToDatabase };
 const mongoose = require('mongoose');
 
 async function connectToDatabase() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/abcd"
+    await mongoose.connect("mongodb+srv://rajan:fddGgOBLH8BCfLgJ@cluster0.d8girz9.mongodb.net/studentcrudmm?retryWrites=true&w=majority"
    );
     console.log('Connected to MongoDB');
   } catch (error) {
